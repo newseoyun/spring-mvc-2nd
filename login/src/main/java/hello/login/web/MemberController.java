0e092573e63e7a50a4fe3b43c42105adfcd1d24d
seoyun <newseoyun@gmail.com>
2021-11-15 14:56:55 +0900

    addMember

---
+++ login/src/main/java/hello/login/web/MemberController.java
@@ -0,0 +1,11 @@
+package hello.login.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+}
