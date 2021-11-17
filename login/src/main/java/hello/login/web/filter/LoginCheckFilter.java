8c3d423f9bbcf3252f63703f414b5a2336d349f6
seoyun <newseoyun@gmail.com>
2021-11-17 11:15:39 +0900

    loginCheckFilter

---
+++ login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -0,0 +1,54 @@
+package hello.login.web.filter;
+
+import hello.login.web.session.SessionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Slf4j
+public class LoginCheckFilter implements Filter {
+
+    private static final String[] whitelist = {"/", "/members/add", "/login", "/logout", "/css/*"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        try {
+            log.info("인증 체크 필터 시작 {}", requestURI);
+
+            if (isLoginCheckPath(requestURI)) {
+                log.info("인층 체크 로직 실행 {}", requestURI);
+                HttpSession session = httpRequest.getSession(false);
+                if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+                    log.info("미인증 사용자 요청 {}", requestURI);
+
+                    // 로그인으로 redirect
+                    httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
+                    // 로그인에서 requestURI 로 이동할 수 있도록 redirectURL 정보를 같이 넘겨준 것.
+                    return;
+                }
+            }
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e; // 로그만 찍어도 되지만 정상동작한 것처럼 보일 수 있으므로 톰캣까지 예외를 올려주기 위해 throw
+        } finally {
+            log.info("인증 체크 필터 종료");
+        }
+    }
+
+    /**
+     * 화이트리스트는 인증 체크 X
+     */
+    private boolean isLoginCheckPath(String requestURI) {
+        return PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    }
+}
